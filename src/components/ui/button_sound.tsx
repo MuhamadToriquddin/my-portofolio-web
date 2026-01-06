92c9bc34ccf621e9e80d4b7749dab15570631f56
Muhamad Toriquddin <toriqu.san@gmail.com>
2026-01-06 22:36:32 +0700

    update:button sound

--- src/components/ui/button_sound.tsx
+++ src/components/ui/button_sound.tsx
@@ -3,7 +3,7 @@ import soundOn from "../../assets/icon/sound on.png";
 
 function ButtonSound() {
   return (
-    <button className="border-4 border-black p-1 hover:bg-gray-100 transition-colors">
+    <button className="border-4 hover:border-black hover:bg-gray-100  p-1 border-slate-500 transition-colors">
       {/* Gunakan tag img dan perbesar ukurannya, misal w-8 (32px) */}
       <img src={soundOn} alt="Sound On" className="w-8 h-8 object-contain" />
     </button>
