94f24f9ff3ba2d1ed665bcd2a0256326903ac29f
Muhamad Toriquddin <toriqu.san@gmail.com>
2026-01-12 20:09:02 +0700

    add sound to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,5 @@
+import useSound from "use-sound"
+import clickSound from "../../assets/audio/click.mp3"
 interface buttonProps{
     onClick?:()=>void,
     label:string,
@@ -9,8 +11,16 @@ interface buttonProps{
 export function Button (
     {onClick,label,type,disabled,size}:buttonProps
 ){
+  const [playClick]=useSound(clickSound,{volume:0.2})
+    const click = ()=>{
+        if (onClick){
+            onClick()
+            playClick()
+        }
+        playClick()
+    }
     return(
-        <button className={`font-paragraph hover:scale-110 transition-all px-3 ${size == "sm"?"text-2xl":"text-3xl "}`} onClick={()=>onClick} disabled={disabled} type={type as React.ButtonHTMLAttributes<HTMLButtonElement>['type']}>
+        <button className={`font-paragraph hover:scale-110 transition-all px-3 ${size == "sm"?"text-2xl":"text-3xl "}`} onClick={click} disabled={disabled} type={type as React.ButtonHTMLAttributes<HTMLButtonElement>['type']}>
             [ {label} ]
         </button>
     )
